11afd44bbac18d1fdd7fdca92a2f962152c1c2f6
jiji <jijigopal2000@gmail.com>
2021-09-09 16:30:24 +0530

    msg

---
+++ MEA(R)N/JavaScript/script.js
@@ -0,0 +1,10 @@
+// In JavaScript To print a message in console 
+
+// console.log();
+
+var name="Jijina";
+var age=21;
+console.log(name);
+console.log(age);
+
+console.log("Hi, I am "+name+","+age+" years old.");
